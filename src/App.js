64110105ef6e00f1354a56d76626b347f120bf6b
Daniel Almeida <daniel_al.santos@hotmail.com>
2021-01-26 13:44:09 -0300

    Ajuste do logo, da fonte e das cores; Link para as paginas

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
 
+import Home from "./Pages/Home";
+import Contato from "./Pages/Contato";
+import Servicos from "./Pages/Servicos";
+import Portifolio from "./Pages/Portifolio";
+
 function App() {
   return (
     <Router>
       <Navbar />
       <Switch>
-        <Route path="/" />
+        <Route path="/" exact component={Home} />
+        <Route path="/portifolio" exact component={Portifolio} />
+        <Route path="/servicos" exact component={Servicos} />
+        <Route path="/contato" exact component={Contato} />
       </Switch>
     </Router>
   );
--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "../Button";
 import { FaBars, FaTimes, FaCameraRetro } from "react-icons/fa";
+import { RiCameraLensFill } from "react-icons/ri";
 import "./Navbar.css";
 
 function Navbar() {
@@ -11,17 +12,21 @@ function Navbar() {
 
   const handleClick = () => setClick(!click);
 
-  console.log(click);
-
   const closeMobileMenu = () => setClick(false);
+
+  //Função para mostrar o botão quando estiver em outros dispositivos com uma resolução menor que 960
 
   const showButton = () => {
     if (window.innerWidth <= 960) {
+      //verifica se o tamanho interno da janela é menor ou igual a 960
       setButton(false);
     } else {
       setButton(true);
     }
   };
+
+  //Adicionado um evento a janela, que monitora o redimensionamento e chama uma função
+  //O botão aparece caso o redimensionamento bata com o descrito na função
 
   window.addEventListener("resize", showButton);
 
@@ -29,7 +34,7 @@ function Navbar() {
     <div className="navbar">
       <div className="navbar-container container">
         <Link to="/" className="navbar-logo">
-          <FaCameraRetro className="navbar-icon" />
+          <RiCameraLensFill className="navbar-icon" />
           Phin Photos
         </Link>
 
@@ -38,35 +43,51 @@ function Navbar() {
         </div>
         <ul className={click ? "nav-menu active" : "nav-menu"}>
           <li className="nav-item">
-            <Link to="/" className="nav-links">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
               Home
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/portifolio" className="nav-links">
+            <Link
+              to="/portifolio"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
               Portifolio
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/servicos" className="nav-links">
+            <Link
+              to="/servicos"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
               Serviços
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/contato" className="nav-links">
+            <Link to="/contato" className="nav-links" onClick={closeMobileMenu}>
               Contato
             </Link>
           </li>
           <li className="nav-btn">
             {button ? (
               <Link to="/signup" className="btn-link">
-                <Button buttonStyle="btn--outline" buttonColor="btn--outline">
+                <Button
+                  buttonStyle="btn--outline"
+                  buttonColor="btn--outline"
+                  onClick={closeMobileMenu}
+                >
                   SIGN UP
                 </Button>
               </Link>
             ) : (
               <Link to="/signup" className="btn-link">
-                <Button buttonStyle="btn--medium" buttonSize="btn--mobile">
+                <Button
+                  buttonStyle="btn--medium"
+                  buttonSize="btn--mobile"
+                  onClick={closeMobileMenu}
+                >
                   SIGN UP
                 </Button>
               </Link>
